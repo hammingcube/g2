0a4f93b5bae521708ab4aade3b07af69138ffcd2
maddyonline <madhavjha@gmail.com>
2016-08-18 02:42:05 -0700

    more

--- client-app/static/app.js
+++ client-app/static/app.js
@@ -1,13 +1,13 @@
 function showStartCodingLink(data) {
   var el = document.createElement('a');
+  el.id = "child";
   el.href = "cui/"+ data.ticket_id;
   el.target = "_blank";
   el.appendChild(document.createTextNode("Click to solve " + data.problem_id));
-  var parent = document.getElementById('dest');
-  for(i=0; i < parent.childNodes.length; i++) {
-    parent.removeChild(parent.childNodes[i]);
-  }
-  parent.appendChild(el);
+  var parent = document.getElementById('parent');
+  var child = document.getElementById('child');
+  parent.replaceChild(el, child);
+
 }
 
 function createNewTicket(problemId) {
@@ -30,7 +30,6 @@ function createNewTicket(problemId) {
 
 $(document).ready(function() {
   $('.newproblem').each(function(i, el){
-    console.log(el.name);
     el.addEventListener('click', function(){
       createNewTicket(el.name);
     });
